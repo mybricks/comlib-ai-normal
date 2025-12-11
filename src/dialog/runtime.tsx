372e156b3c6bb157a40828fe4a95c53a890cd273
leon-js <42924541+leon-js@users.noreply.github.com>
2025-12-11 18:45:13 +0800

    update

--- src/dialog/runtime.tsx
+++ src/dialog/runtime.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useMemo } from 'react'
 import css from './runtime.less'
 
 interface Message {
